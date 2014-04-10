bbe4eaba35b516df59b40d9c7cfd33c661d1aa16
gcav <gcav@users.noreply.github.com>
2014-04-09 21:58:05 -0400

    adding stub file for EN config localizations (not populated)

---
+++ lang/en/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * configuration manager labels
+ * https://www.dokuwiki.org/devel:configuration#configuration_metadata
+ * 
+ * this will accept html for formatting ...
+ *
+    $lang['site_name'] = "<br /><i>test</i>";
+ *
+ *
+ * /
+
